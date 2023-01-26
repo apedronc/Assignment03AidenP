6d327499f9045464641dcb59b6f9b5ab76c4daf7
apedronc <apedronc@iu.edu>
2023-01-26 16:47:39 -0500

    wow

--- Assets/Scenes/BallController1.cs
+++ Assets/Scenes/BallController1.cs
@@ -19,7 +19,7 @@ public class BallController : MonoBehaviour
 
         originalPosition = new Vector3[ball.Length];
         originalRotation = new Quaternion[ball.Length];
-        ballSpeed = new float[ball.Length];
+        ballSpeed = new float [ball.Length];
 
 
 
@@ -30,7 +30,13 @@ public class BallController : MonoBehaviour
 
         }
 
- 
+        for (int i = 0; i < ball.Length; i++) {
+
+            ballSpeed[i] = Random.Range(1f, 10f);
+
+        }
+
+       
 
 
     }
@@ -38,16 +44,19 @@ public class BallController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal")
-        float vertical = Input.GetAxis("vertical")
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
 
         Vector3 direction = new Vector3(horizontal, 0, vertical);
 
-        direction = direction * Time.deltaTime * ballSpeed;
+        for (int i = 0; i > 10; i++) { 
+      
+            direction = direction * Time.deltaTime * ballSpeed;
 
-        ball.localPosition = ball.localPosition + direction;
+            ball.localPosition = ball.localPosition + direction;
+        }
 
-        Input.GetButtonDown("Jump")
+        Input.GetButtonDown("Jump");
 
     }
 }
